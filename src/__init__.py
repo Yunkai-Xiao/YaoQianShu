fa65866ba6a65f92f27cce5550cd87b7a1205982
Yunkai Xiao <38600058+Yunkai-Xiao@users.noreply.github.com>
2025-06-03 21:47:38 -0700

    Add backtesting engine and strategy interface

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+from .data import DataStore, DataPortal, download_history
+from .engine import Engine
+from .strategy import Strategy
+
+__all__ = [
+    "DataStore",
+    "DataPortal",
+    "download_history",
+    "Engine",
+    "Strategy",
+]
